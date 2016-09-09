0e900d9c1c024c957ea46d9d5b7842214b894558
Frederico Jordan <fredericojordan@gmail.com>
2016-09-09 18:59:28 -0300

    Resizable screen, C hotkey to change color, printing exceptions

--- gui.py
+++ gui.py
@@ -5,10 +5,12 @@ Created on 2 de set de 2016
 '''
 import pygame, chess
 from random import choice
+from traceback import format_exc
+from sys import stderr
+from time import strftime
 pygame.init()
 
 SQUARE_SIDE = 50
-SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT) = (8*SQUARE_SIDE, 8*SQUARE_SIDE)
 
 CHECK_RED   = (240, 150, 150)
 
@@ -25,28 +27,34 @@ BOARD_COLORS = [(LIGHT_GRAY, DARK_GRAY),
 BOARD_COLOR = choice(BOARD_COLORS)
 
 
-BLACK_KING   = pygame.transform.scale(pygame.image.load('images/black_king.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
-BLACK_QUEEN  = pygame.transform.scale(pygame.image.load('images/black_queen.png'),  (SQUARE_SIDE,SQUARE_SIDE) )
-BLACK_ROOK   = pygame.transform.scale(pygame.image.load('images/black_rook.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
-BLACK_BISHOP = pygame.transform.scale(pygame.image.load('images/black_bishop.png'), (SQUARE_SIDE,SQUARE_SIDE) )
-BLACK_KNIGHT = pygame.transform.scale(pygame.image.load('images/black_knight.png'), (SQUARE_SIDE,SQUARE_SIDE) )
-BLACK_PAWN   = pygame.transform.scale(pygame.image.load('images/black_pawn.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
+BLACK_KING   = pygame.image.load('images/black_king.png')
+BLACK_QUEEN  = pygame.image.load('images/black_queen.png')
+BLACK_ROOK   = pygame.image.load('images/black_rook.png')
+BLACK_BISHOP = pygame.image.load('images/black_bishop.png')
+BLACK_KNIGHT = pygame.image.load('images/black_knight.png')
+BLACK_PAWN   = pygame.image.load('images/black_pawn.png')
 
-WHITE_KING   = pygame.transform.scale(pygame.image.load('images/white_king.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
-WHITE_QUEEN  = pygame.transform.scale(pygame.image.load('images/white_queen.png'),  (SQUARE_SIDE,SQUARE_SIDE) )
-WHITE_ROOK   = pygame.transform.scale(pygame.image.load('images/white_rook.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
-WHITE_BISHOP = pygame.transform.scale(pygame.image.load('images/white_bishop.png'), (SQUARE_SIDE,SQUARE_SIDE) )
-WHITE_KNIGHT = pygame.transform.scale(pygame.image.load('images/white_knight.png'), (SQUARE_SIDE,SQUARE_SIDE) )
-WHITE_PAWN   = pygame.transform.scale(pygame.image.load('images/white_pawn.png'),   (SQUARE_SIDE,SQUARE_SIDE) )
+WHITE_KING   = pygame.image.load('images/white_king.png')
+WHITE_QUEEN  = pygame.image.load('images/white_queen.png')
+WHITE_ROOK   = pygame.image.load('images/white_rook.png')
+WHITE_BISHOP = pygame.image.load('images/white_bishop.png')
+WHITE_KNIGHT = pygame.image.load('images/white_knight.png')
+WHITE_PAWN   = pygame.image.load('images/white_pawn.png')
 
 CLOCK = pygame.time.Clock()
 CLOCK_TICK = 15
 
-SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+SCREEN = pygame.display.set_mode((8*SQUARE_SIDE, 8*SQUARE_SIDE), pygame.RESIZABLE)
 SCREEN_TITLE = 'Chess Game'
 
 pygame.display.set_icon(pygame.image.load('images/chess_icon.ico'))
 pygame.display.set_caption(SCREEN_TITLE)
+
+def resize_screen(square_side_len):
+    global SQUARE_SIDE
+    global SCREEN
+    SCREEN = pygame.display.set_mode((8*square_side_len, 8*square_side_len), pygame.RESIZABLE)
+    SQUARE_SIDE = square_side_len
 
 def print_empty_board():
     SCREEN.fill(BOARD_COLOR[0])
@@ -88,30 +96,30 @@ def print_board(board, color=chess.WHITE):
         paint_square(chess.bb2str(chess.get_king(board, chess.BLACK)), CHECK_RED)
     
     for position in chess.colored_piece_gen(board, chess.KING, chess.BLACK):
-        SCREEN.blit(BLACK_KING, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_KING,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.QUEEN, chess.BLACK):
-        SCREEN.blit(BLACK_QUEEN, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_QUEEN,  (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.ROOK, chess.BLACK):
-        SCREEN.blit(BLACK_ROOK, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_ROOK,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.BISHOP, chess.BLACK):
-        SCREEN.blit(BLACK_BISHOP, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_BISHOP, (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.KNIGHT, chess.BLACK):
-        SCREEN.blit(BLACK_KNIGHT, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_KNIGHT, (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.PAWN, chess.BLACK):
-        SCREEN.blit(BLACK_PAWN, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(BLACK_PAWN,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
         
     for position in chess.colored_piece_gen(board, chess.KING, chess.WHITE):
-        SCREEN.blit(WHITE_KING, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_KING,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.QUEEN, chess.WHITE):
-        SCREEN.blit(WHITE_QUEEN, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_QUEEN,  (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.ROOK, chess.WHITE):
-        SCREEN.blit(WHITE_ROOK, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_ROOK,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.BISHOP, chess.WHITE):
-        SCREEN.blit(WHITE_BISHOP, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_BISHOP, (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.KNIGHT, chess.WHITE):
-        SCREEN.blit(WHITE_KNIGHT, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_KNIGHT, (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
     for position in chess.colored_piece_gen(board, chess.PAWN, chess.WHITE):
-        SCREEN.blit(WHITE_PAWN, get_square_rect(chess.bb2str(position)))
+        SCREEN.blit(pygame.transform.scale(WHITE_PAWN,   (SQUARE_SIDE,SQUARE_SIDE)), get_square_rect(chess.bb2str(position)))
         
     pygame.display.flip()
     
@@ -179,10 +187,25 @@ def play_as(game, color):
                         set_title(SCREEN_TITLE)
                         print_board(game.board, color)
                         ongoing = True
+                    if event.key == 99: # C key
+                        global BOARD_COLOR
+                        BOARD_COLOR = choice(BOARD_COLORS)
+                        print_board(game.board, color)
+                
+                if event.type == pygame.VIDEORESIZE:
+                    if SCREEN.get_height() != event.h:
+                        resize_screen(int(event.h/8.0))
+                    elif SCREEN.get_width() != event.w:
+                        resize_screen(int(event.w/8.0))
+                    print_board(game.board, color)
     except:
+        print(format_exc(), file=stderr)
         bug_file = open('bug_report.txt', 'a')
+        bug_file.write('----- ' + strftime('%x %X') + ' -----\n')
+        bug_file.write(format_exc() + '\n')
         bug_file.write('\n'.join(game.position_history))
-        bug_file.write('\n\n')
+        bug_file.write('\n-----------------------------\n')
+        bug_file.write('\n')
         bug_file.close()
 
 def play_as_white(game=chess.Game()):
